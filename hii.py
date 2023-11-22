159cdb41601fc32c2cfe1b7cda75488e93396be6
Abhisek bharti <abhisek.bharti@rungta.org>
2023-11-23 00:15:21 +0530

    new work

---
+++ hii.py
@@ -0,0 +1,9 @@
+print("hello wold")
+name ="abhisek"
+age ="22"
+print(name,age)
+name=input("what is your name")
+print(age.upper())
+
+
+
---
+++ work.py
@@ -0,0 +1,12 @@
+
+print("hello")
+import numpy as np
+
+arr=np.array([1,2,3,4,5,6])
+
+
+
+print(arr)
+print([arr.shape])
+array=arr.transpose()
+
